1c12340d3a95d80348c3d940c174173fd5a62198
Uriel Mori <moriuriel@gmail.com>
2024-08-20 12:54:35 -0300

    feat: added factory

---
+++ src/Wallet.Domain/Factories/WalletFactory/IWalletFactory.cs
@@ -0,0 +1,11 @@
+﻿using Wallets.Domain.Entities.Interfaces;
+using Wallets.Domain.ValueObjects;
+
+namespace Wallets.Domain.Factories.WalletFactory;
+public interface IWalletFactory
+{
+    IWallet Construct(
+        AccountHolder accountHolder,
+        Account account,
+        float balance);
+}
---
+++ src/Wallet.Domain/Factories/WalletFactory/WalletFactory.cs
@@ -0,0 +1,14 @@
+﻿using Wallets.Domain.Entities;
+using Wallets.Domain.Entities.Interfaces;
+using Wallets.Domain.ValueObjects;
+
+namespace Wallets.Domain.Factories.WalletFactory;
+
+public class WalletFactory : IWalletFactory
+{
+    public IWallet Construct(
+        AccountHolder accountHolder,
+        Account account,
+        float balance)
+    => Wallet.Create(accountHolder, account, balance);
+}
