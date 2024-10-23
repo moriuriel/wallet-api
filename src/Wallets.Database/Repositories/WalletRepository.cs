cd2f51c82f1752456cdd96058aac6859a2a6e63a
Uriel Mori <moriuriel@gmail.com>
2024-10-23 12:45:49 -0300

    feat: implement repository method

--- src/Wallets.Database/Repositories/WalletRepository.cs
+++ src/Wallets.Database/Repositories/WalletRepository.cs
@@ -1,3 +1,6 @@
+using System.Data;
+using Dapper;
+using Wallets.Database.Mapper;
 using Wallets.Domain.Entities.Interfaces;
 using Wallets.Domain.Interfaces;
 
@@ -5,17 +8,53 @@ namespace Wallets.Database.Repositories;
 
 public sealed class WalletRepository : IWalletRepository
 {
+  private readonly IDbConnection _dbConnection;
+
+  public WalletRepository(IDbConnection dbConnection)
+    => _dbConnection = dbConnection;
+
   public Task InsertAsync(
     IWallet wallet,
     CancellationToken cancellationToken)
   {
-    throw new NotImplementedException();
+    var rawSql = @"INSERT INTO public.wallets
+                (id, name, tax_id, account_branch, account_number, created_at)
+                VALUES
+                (@id, @name, @taxId, @accountBranch, @accountNumber, @createdAt)";
+
+    var mapper = WalletMapper.FactoryByEntity(wallet);
+
+    var command = new CommandDefinition(
+      commandText: rawSql,
+      parameters: new 
+      { 
+        id = mapper.Id,
+        name = mapper.Name,
+        taxId = mapper.TaxId,
+        accountBranch = mapper.AccountBranch,
+        accountNumber = mapper.AccountNumber,
+        createdAt = mapper.CreatedAt,
+      },
+      cancellationToken: cancellationToken
+    );
+
+    return _dbConnection.ExecuteAsync(command);
   }
 
-  public Task<bool> IsExistsAccountHolderAsync(
+  public async Task<bool> IsExistsAccountHolderAsync(
     string taxId,
     CancellationToken cancellationToken)
   {
-    throw new NotImplementedException();
+    var rawSql = $@"SELECT COUNT(tax_id) FROM public.wallets WHERE tax_id = @{nameof(taxId)}";
+
+    var command = new CommandDefinition(
+      commandText: rawSql,
+      parameters: new { taxId },
+      cancellationToken: cancellationToken
+    );
+
+    int total = await _dbConnection.ExecuteScalarAsync<int>(command);
+
+    return total > uint.MinValue;
   }
 }
