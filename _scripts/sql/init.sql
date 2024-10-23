7800f3c05fd39f78ae787831aa24c21a3c49fb8d
Uriel Mori <moriuriel@gmail.com>
2024-10-23 12:45:38 -0300

    chore: fixing sql script

--- _scripts/sql/init.sql
+++ _scripts/sql/init.sql
@@ -1,20 +1,20 @@
-CREATE TABLE IF NOT EXISTS Wallets(
-  id varchar(32) not null primary key,
+CREATE TABLE IF NOT EXISTS wallets(
+  id varchar(36) not null primary key,
   name varchar(100) not null,
-  taxId varchar(14) not null,
-  accountBranch varchar(2) not null,
-  accountNumber varchar(6) not null,
+  tax_id varchar(14) not null,
+  account_branch varchar(2) not null,
+  account_number varchar(6) not null,
   balance  decimal(20, 2) null,
-  createdAt TIMESTAMP not null
+  created_at TIMESTAMP not null
 );
 
-CREATE TABLE IF NOT EXISTS Transactions(
-  id varchar(32) not null primary key,
-  payerId varchar(32) not null,
-  receiverId varchar(32) not null,
+CREATE TABLE IF NOT EXISTS transactions(
+  id varchar(36) not null primary key,
+  payer_id varchar(36) not null,
+  receiver_id varchar(36) not null,
   amount  decimal(20, 2) null,
-  transactionDate TIMESTAMP not null,
-  createdAt TIMESTAMP not null,
-  FOREIGN KEY (payerId) REFERENCES Wallets(id),
-  FOREIGN KEY (receiverId) REFERENCES Wallets(id)
+  transaction_date TIMESTAMP not null,
+  created_at TIMESTAMP not null,
+  FOREIGN KEY (payer_id) REFERENCES Wallets(id),
+  FOREIGN KEY (receiver_id) REFERENCES Wallets(id)
 );
